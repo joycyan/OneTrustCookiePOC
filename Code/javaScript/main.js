d53d40e6edbc900e9cf29128578a0e30b14e267a
mariwilliams2024 <romariorwilliams@gmail.com>
2024-07-09 10:09:53 -0400

    login and signout functionality added

--- Code/javaScript/main.js
+++ Code/javaScript/main.js
@@ -1,5 +1,5 @@
 document.getElementById("demo").onclick = function pageLoad() {
-    location.href = "../html/subpage.html";
+  location.href = "../html/subpage.html";
 };
 
 document.getElementById("user").onclick = function pageLoadUser() {
@@ -9,25 +9,62 @@ document.getElementById("user").onclick = function pageLoadUser() {
 // WORKAROUND: OneTrust doesn't work consistently
 document.getElementById("otbutton").onclick = function infoDisplay() {
   // Display manually the cookie settings
-  window.OneTrust.ToggleInfoDisplay()
+  window.OneTrust.ToggleInfoDisplay();
 };
 
-document.getElementById('price').innerHTML =  document.cookie.match('(^|;)\\s*' + "Unit" + '\\s*=\\s*([^;]+)')?.pop() || 'no_value_storeded';
+document.getElementById("price").innerHTML =
+  document.cookie.match("(^|;)\\s*" + "Unit" + "\\s*=\\s*([^;]+)")?.pop() ||
+  "no_value_storeded";
 
-const str = document.cookie.match('(^|;)\\s*' + "Email" + '\\s*=\\s*([^;]+)')?.pop() || 'no_value_stored';
+const str =
+  document.cookie.match("(^|;)\\s*" + "Email" + "\\s*=\\s*([^;]+)")?.pop() ||
+  "no_value_stored";
 
-document.getElementById('username').innerHTML = decodeURI(str);
+document.getElementById("username").innerHTML = decodeURI(str);
 
-document.getElementById('pageview').innerHTML = document.cookie.match('(^|;)\\s*' + "pageviewCount" + '\\s*=\\s*([^;]+)')?.pop() || 'no_value_stored';
+document.getElementById("pageview").innerHTML =
+  document.cookie
+    .match("(^|;)\\s*" + "pageviewCount" + "\\s*=\\s*([^;]+)")
+    ?.pop() || "no_value_stored";
 
+console.log("logging consent changes: ", window.OnetrustActiveGroups);
 
-  console.log("logging consent changes: ", window.OnetrustActiveGroups);
+const changeLang = (languageCode) => {
+  document.documentElement.setAttribute("lang", languageCode);
+};
 
-  const changeLang = (languageCode) => {
-    document.documentElement.setAttribute("lang", languageCode);
-  };
+//START login and logout functionalities
 
+function getStoredLoginEmail() {
+  return localStorage.getItem("loginEmail");
+}
 
+function displayAuthButton() {
+  const authenticationBtnContainer = document.getElementById(
+    "authenticationBtnContainer"
+  );
+  const loginEmail = getStoredLoginEmail();
+  if (loginEmail) {
+    authenticationBtnContainer.innerHTML = `${loginEmail} <button type="button" class="mb-3" onclick="signOut()">Sign out</button>`;
+  } else {
+    authenticationBtnContainer.innerHTML = `<button type="button" class="mb-3" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>`;
+  }
+}
 
+function signOut() {
+  localStorage.removeItem("loginEmail");
+  location.reload();
+}
 
+document.addEventListener("DOMContentLoaded", displayAuthButton);
 
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var loginEmail = document.getElementById("loginEmail").value;
+  localStorage.setItem("loginEmail", loginEmail);
+  location.reload();
+});
+
+//END login and logout functionalities
